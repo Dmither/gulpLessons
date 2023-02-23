a3f33c0f0bcfed8a72a074535568e2d2747c09ec
Dmither <mr.d.graumann@gmail.com>
2023-02-23 12:53:34 +0200

    update gulp

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,13 +1,13 @@
 /* Плагіни, які імпортуються глобально для кількох завдань */
 
 import replace from "gulp-replace"; // пошук і заміна
+import browserSync from "browser-sync"; // оновлення браузера
 import gulpPlumber from "gulp-plumber"; // обробка помилок
 import notify from "gulp-notify"; // сповіщення
-import browserSync from "browser-sync"; // оновлення браузера
 
 export const plugins = {
-  replace: replace,
-  plumber: gulpPlumber,
-  notify: notify,
-  browserSync: browserSync,
-}
+	replace: replace,
+	plumber: gulpPlumber,
+	notify: notify,
+	browserSync: browserSync,
+};
